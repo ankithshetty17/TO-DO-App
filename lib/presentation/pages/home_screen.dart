eabf4c9e545622f0dbec677480302219582084b9
ankithshetty17 <ankishetty2002@gmail.com>
2024-08-06 12:27:09 +0530

    top-bar

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -10,10 +10,50 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return DefaultTabController(
+      length: 3,
+     child:Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.purple[400],
+        bottom: TabBar(
+         tabs: [
+          Tab(
+            text: 'All',
+          ),
+            Tab(
+            text: 'Completed',
+          ),
+            Tab(
+            text: 'Pending',
+          ),
+        ],
+      
+        ),
+        title:const Text('TO-DO APP',style: TextStyle(fontWeight: FontWeight.bold),),
       ),
-    );
+      body: TabBarView(
+        children:[
+          Container(
+            color: Colors.yellow,
+            child: Text('All'),
+          ),
+           Container(
+            color: Colors.green,
+            child: Text('Completed'),
+          ),
+           Container(
+            color: Colors.red,
+            child: Text('Pending'),
+          )
+        ] 
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){},
+          child: Icon(Icons.add), 
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+     )
+      );
   }
 }
